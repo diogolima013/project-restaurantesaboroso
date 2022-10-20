3b5afc9fa02d0003e18d8e762c68a8346136cde8
Diogo Lima <diogoslva013@gmail.com>
2022-10-20 11:32:00 -0300

    registering form in the database

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,24 +1,21 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require ('./../inc/menus')
+var reservations = require('./../inc/reservations');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `, (err, results)=>{
-
-    if (err) {
-      console.log(err);
-    }
-
+  menus.getMenus().then(results =>{
+    
     res.render('index', { 
       title: 'Restaurante Saboroso!',
-      menus: results
+      menus: results,
+      isHome:true
     });
 
-  }); 
+  });
 
 });
 
@@ -34,21 +31,52 @@ router.get('/contacts', function(req, res, next){
 
 router.get('/menus', function(req, res, next){
 
-  res.render('menus' , {
-    title: 'Menu - Restaurante Saboroso!',
-    background: 'images/img_bg_1.jgp',
-    h1 : 'Saboreie nosso menu!'
-  } );
+  menus.getMenus().then(results=>{
+
+    res.render('menus' , {
+      title: 'Menu - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jgp',
+      h1 : 'Saboreie nosso menu!',
+      menus: results
+    });
+
+  });
 
 });
 
 router.get('/reservations', function(req, res, next){
 
-  res.render('reservations' , {
-    title: 'Reserva - Restaurante Saboroso!',
-    background: 'images/img_bg_2.jgp',
-    h1 : 'Reserve uma Mesa!'
-  });
+  reservations.render(req, res);
+
+});
+
+router.post('/reservations', function(req, res, next){
+
+  if (!req.body.name){
+    reservations.render(req, res, "Digite o nome" );
+  } else if (!req.body.email){
+    reservations.render(req, res, "Digite o email" );
+  } else if (!req.body.people){
+    reservations.render(req, res, "Digite o numero de pessoas" );
+  } else if (!req.body.date){
+    reservations.render(req, res, "Selecione a data" );
+  } else if (!req.body.time){
+    reservations.render(req, res, "Selecione a hora" );
+  } else {
+
+    reservations.save(req.body).then(results =>{
+
+      req.body = {};
+
+      reservations.render(req, res, null, 'Reserva realizada com sucesso!' );
+
+    }).catch(err=>{
+
+      reservations.render(req, res, err.message);
+
+    });
+
+  }
 
 });
 
