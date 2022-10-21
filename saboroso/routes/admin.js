307edfa132cebe0f90672b495af5991438f66efd
Diogo Lima <diogoslva013@gmail.com>
2022-10-21 09:26:57 -0300

    shaping the menu

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -1,5 +1,6 @@
 var express = require ('express');
 var users = require('./../inc/users');
+var admin = require('./../inc/admin')
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -9,6 +10,14 @@ router.use(function(req, res, next){
     } else {
         next();
     }
+});
+
+router.use(function(req, res, next){
+
+    req.menus = admin.getMenus(req);
+
+    next();
+
 });
 
 router.get('/logout', function(req, res, next){
@@ -21,7 +30,9 @@ router.get('/logout', function(req, res, next){
 
 router.get('/', function(req, res, next){
 
-    res.render('admin/index');
+    res.render('admin/index',{
+        menus: req.menus
+    });
 
 });
 
@@ -57,21 +68,27 @@ router.get('/login', function(req, res, next){
 
 router.get('/contacts', function(req, res, next){
 
-    res.render('admin/contacts');
+    res.render('admin/contacts',{
+        menus: req.menus
+    });
 
 
 });
 
 router.get('/emails', function(req, res, next){
 
-    res.render('admin/emails');
+    res.render('admin/emails',{
+        menus: req.menus
+    });
 
 
 });
 
 router.get('/menus', function(req, res, next){
 
-    res.render('admin/menus');
+    res.render('admin/menus',{
+        menus: req.menus
+    });
 
 
 });
@@ -79,7 +96,8 @@ router.get('/menus', function(req, res, next){
 router.get('/reservations', function(req, res, next){
 
     res.render('admin/reservations', {
-        date:{}
+        date:{},
+        menus: req.menus
     });
 
 
@@ -87,7 +105,9 @@ router.get('/reservations', function(req, res, next){
 
 router.get('/users', function(req, res, next){
 
-    res.render('admin/users');
+    res.render('admin/users',{
+        menus: req.menus
+    });
 
 
 });
