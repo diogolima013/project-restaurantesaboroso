291a1002a70a4abe0ce7e303a7d1d32f5b43ed0e
Diogo Lima <diogoslva013@gmail.com>
2022-10-24 08:40:51 -0300

    formatting the booking date

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -2,8 +2,12 @@ var express = require ('express');
 var users = require('./../inc/users');
 var admin = require('./../inc/admin')
 var menus = require('./../inc/menus')
-var reservations = require('./../inc/reservations')
+var reservations = require('./../inc/reservations');
+var moment = require('moment');
 var router = express.Router();
+
+
+moment.locale('pt-BR');
 
 router.use(function(req, res, next){
 
@@ -131,11 +135,15 @@ router.delete('/menus/:id', function(req, res, next){
 
 router.get('/reservations', function(req, res, next){
 
-    res.render('admin/reservations', admin.getParams(req,{
-        date: {}
-    }));
+    reservations.getReservations().then(data=> {
 
+        res.render('admin/reservations', admin.getParams(req,{
+            date: {},
+            data,
+            moment
+        }));
 
+    });
 });
 
 router.post('/reservations', function(req, res, next){
